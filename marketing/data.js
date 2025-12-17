3232d90676d762f7c2b44f23f55378d6e6027f3a
James Henry Lobo Morais <j4mesmorais@gmail.com>
2025-12-17 16:52:27 -0300

    Adiciona Marketin Pistori

---
+++ marketing/data.js
@@ -0,0 +1,35 @@
+const produtos = [
+  {
+    nome: "Quinta do Cerrado",
+    descricao: "Artes para anúncios e outros",
+    imagem: "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg",
+    botoes: [
+      { texto: "Ver agora 01", link: "#" },
+      { texto: "Ver agora 02", link: "#" }
+    ]
+  },
+  {
+    nome: "Casa Praiana",
+    descricao: "Artes para anúncios e outros",
+    imagem: "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg",
+    botoes: [
+      { texto: "Ver agora", link: "#" }
+    ]
+  },
+  {
+    nome: "Figueira",
+    descricao: "Artes para anúncios e outros",
+    imagem: "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg",
+    botoes: [
+      { texto: "Ver agora", link: "#" }
+    ]
+  },
+  {
+    nome: "Casa Uva",
+    descricao: "Artes para anúncios e outros",
+    imagem: "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg",
+    botoes: [
+      { texto: "Ver agora", link: "#" }
+    ]
+  }
+];
