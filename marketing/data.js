8c3bc7f1c261c6b43aae799ac2b3594a5509e3be
James Henry Lobo Morais <j4mesmorais@gmail.com>
2025-12-18 16:44:58 -0300

    Inclusão de produtos

--- marketing/data.js
+++ marketing/data.js
@@ -1,35 +1,72 @@
 const produtos = [
   {
-    nome: "Quinta do Cerrado",
+    nome: "Quintas do Cerrado",
     descricao: "Artes para anúncios e outros",
-    imagem: "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg",
+    imagem: "./img/QuintasCerrado.jpg",
     botoes: [
-      { texto: "Ver agora 01", link: "#" },
-      { texto: "Ver agora 02", link: "#" }
+      { texto: "Ver agora 01", link: "https://drive.google.com/drive/folders/1iGaUcww_cwOuPVmHB5JIXM2OrGUyLW_w" },
+      { texto: "Ver agora 02", link: "https://drive.google.com/drive/folders/1BSrsUOgu0wZy-SUiu874lxm9oMtdPLhS" }
+    ]
+  },
+   
+  {
+    nome: "Terra Vermelha",
+    descricao: "Artes para anúncios e outros",
+    imagem: "./img/IconeTerraVermelha.png",
+    botoes: [
+      { texto: "Ver agora", link: "https://drive.google.com/drive/folders/1P8Wjs66XEXEMP-10W0A_5xBcG7A6FRuW" }
     ]
   },
   {
-    nome: "Casa Praiana",
+    nome: "Portal do Lago",
     descricao: "Artes para anúncios e outros",
-    imagem: "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg",
+    imagem: "./img/PortalLago.jpg",
     botoes: [
-      { texto: "Ver agora", link: "#" }
-    ]
-  },
-  {
-    nome: "Figueira",
-    descricao: "Artes para anúncios e outros",
-    imagem: "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg",
-    botoes: [
-      { texto: "Ver agora", link: "#" }
+      { texto: "Ver agora", link: "https://drive.google.com/drive/folders/1HXRNytS0-yTrfRCPOPmobC8jgrXsR3Gk" }
     ]
   },
   {
     nome: "Casa Uva",
     descricao: "Artes para anúncios e outros",
-    imagem: "https://kinsta.com/wp-content/uploads/2020/08/tiger-jpg.jpg",
+    imagem: "./img/CasaUva.png",
     botoes: [
-      { texto: "Ver agora", link: "#" }
+      { texto: "Ver agora", link: "https://drive.google.com/drive/folders/1iqIJ14Lupcs6aQa4dqo81JXOSjEXQY5A" }
     ]
-  }
+  },
+  {
+    nome: "Aldeia Santo Antônio",
+    descricao: "Artes para anúncios e outros",
+    imagem: "./img/AldeiaSantoAntonio.jpg",
+    botoes: [
+      { texto: "Ver agora", link: "https://drive.google.com/drive/folders/1woyIemVzZN4b3R9RS1fl0eizLHdhxZaR" }
+    ]
+  },
+  {
+    nome: "Quintas das Araras",
+    descricao: "Artes para anúncios e outros",
+    imagem: "./img/QuintasAraras.png",
+    botoes: [
+      { texto: "Ver agora", link: "https://drive.google.com/drive/folders/1JTH9qKoHi4rtCb6U9fnq4er-qnMj5ErM" }
+    ]
+  },
+  {
+    nome: "Alteza do Lago",
+    descricao: "Artes para anúncios e outros",
+    imagem: "./img/AltezaLago.png",
+    botoes: [
+      { texto: "Ver agora", link: " https://drive.google.com/drive/folders/1-PzQWyAPwfGcs6Mz32ftaFkhWUvKfAj3" }
+    ]
+  },
+  {
+    nome: "Quintas do Lago",
+    descricao: "Artes para anúncios e outros",
+    imagem: "./img/QuintasLago.jpg",
+    botoes: [
+      { texto: "Ver agora", link: "https://drive.google.com/drive/folders/1e-Zf9dtGoqjZR4ApyYKGz7WbhoUAXbfo" }
+    ]
+  }    
+
+
+
+
 ];
